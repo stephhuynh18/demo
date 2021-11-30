b03be54d3910fbd7820d1024f21fc115f1d90b99
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2021-11-30 08:21:21 -0800

    cleanup

--- src/withoutDagJose.ts
+++ src/withoutDagJose.ts
@@ -2,7 +2,10 @@ import { create as createIPFS } from 'ipfs-core';
 import * as dagJose from 'dag-jose';
 
 const stuff = async () => {
-  const ipfs = await createIPFS({ ipld: { codecs: [dagJose] } });
+  const ipfs = await createIPFS();
+
+  console.log();
+  console.log('START\n');
 
   const regularContent = { test: '123' };
   const cid1 = await ipfs.dag.put(regularContent);
@@ -11,16 +14,15 @@ const stuff = async () => {
 
   const cid2 = await ipfs.dag.put(linkedContent);
 
-  console.log((await ipfs.dag.get(cid2)).value, '\n');
-  // {
-  //    link: CID(bafyreiaomrabjvmvcmtr3fsbgpnd3nvbodqh6lexjf3ze4w4gyrxwntno4)
-  // }
+  const full = await ipfs.dag.get(cid2);
+  console.log('expected', linkedContent);
+  console.log('received', full.value);
+  console.log();
 
-  console.log((await ipfs.dag.get(cid2, { path: '/link' })).value, '\n');
-  // { test: '123' }
-
-  console.log((await ipfs.dag.get(cid1)).value, '\n');
-  // { test: '123' }
+  const atPath = await ipfs.dag.get(cid2, { path: '/link' });
+  console.log('expected', regularContent);
+  console.log('recived', atPath.value);
+  console.log();
 };
 
-stuff().then(() => console.log('finished'));
+stuff().then(() => console.log('END'));
